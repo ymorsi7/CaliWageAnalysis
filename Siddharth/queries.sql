2536a221ebd55db3b1bba3442ad5c78c46fd7edc
Siddharth Satyam <siddharth4satyam@gmail.com>
2022-11-18 14:07:14 -0800

    Create queries.sql

---
+++ Siddharth/queries.sql
@@ -0,0 +1,18 @@
+SELECT "Year","Quarter","1st Month Emp","2nd Month Emp","3rd Month Emp"
+FROM
+census
+WHERE 
+"Industry Name" = "Police Protection" and "NAICS Code" = 92212 
+and "Area Name" = "Alameda County"
+and "Ownership" = "State Government"
+and "Quarter" != "Annual";
+
+SELECT "Year","Quarter","1st Month Emp","2nd Month Emp","3rd Month Emp"
+FROM
+census
+WHERE 
+"Industry Name" = "Police Protection" and "NAICS Code" = 92212 
+and "Area Name" = "Alameda County"
+and "Ownership" = "Federal Government"
+and "Quarter" != "Annual";
+
